bcc4d0ff5fe5da70663c1c2738b63e1e5ccfb78b
Moreto23 <93287905+Moreto23@users.noreply.github.com>
2025-11-27 23:41:41 -0500

    Update planes.service.ts

--- frontendgym/src/app/services/planes.service.ts
+++ frontendgym/src/app/services/planes.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { toHttpParams } from './http.util';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class PlanesService {
-  private base = '/api/planes';
+
+  private base = `${environment.apiBaseUrl}/api/planes`;
+
   constructor(private http: HttpClient) {}
-  listar() { return this.http.get<any[]>(`${this.base}`); }
-  obtener(id: number) { return this.http.get<any>(`${this.base}/${id}`); }
+
+  listar() {
+    return this.http.get<any[]>(`${this.base}`);
+  }
+
+  obtener(id: number) {
+    return this.http.get<any>(`${this.base}/${id}`);
+  }
+
   suscribirse(id: number, usuarioId: number, monto?: number) {
-    return this.http.post<any>(`${this.base}/${id}/suscribirse`, null, { params: toHttpParams({ usuarioId, monto }) });
+    return this.http.post<any>(
+      `${this.base}/${id}/suscribirse`,
+      null,
+      { params: toHttpParams({ usuarioId, monto }) }
+    );
   }
 }
