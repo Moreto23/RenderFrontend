08dc4a312a2db29ef1a9d19280964eaca956bf19
Moreto23 <93287905+Moreto23@users.noreply.github.com>
2025-11-27 23:43:24 -0500

    Update suscripciones.service.ts

--- frontendgym/src/app/services/suscripciones.service.ts
+++ frontendgym/src/app/services/suscripciones.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class SuscripcionesService {
-  private base = '/api/suscripciones';
+
+  private base = `${environment.apiBaseUrl}/api/suscripciones`;
+
   constructor(private http: HttpClient) {}
 
   crear(body: {
